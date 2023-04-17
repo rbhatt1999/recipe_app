290a374cf2b6f5f8672c938c3fcca1f9d076dc08
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-17 17:34:03 +0530

    generate recipes

---
+++ spec/views/recipes/show.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "recipes/show", type: :view do
+  before(:each) do
+    assign(:recipe, Recipe.create!(
+      name: "Name",
+      preperation_time: "Preperation Time",
+      cooking_time: "Cooking Time",
+      description: "MyText",
+      public: false,
+      user: nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Preperation Time/)
+    expect(rendered).to match(/Cooking Time/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/false/)
+    expect(rendered).to match(//)
+  end
+end
