767b1e0c6617166205eda559c83344f4ddf94cca
codehass <hassan.elouardy06@gmail.com>
2023-04-19 14:40:33 +0000

    add foods routing test

--- spec/routing/foods_routing_spec.rb
+++ spec/routing/foods_routing_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe FoodsController, type: :routing do
     it 'routes to #index' do
       expect(get: foods_path).to route_to('foods#index')
     end
+
     it 'routes to #new' do
       expect(get: '/foods/new').to route_to('foods#new')
     end
-    it 'routes to #show' do
-      expect(get: food_path('1')).to route_to('foods#show', id: '1')
+
+    it 'routes to #create' do
+      expect(post: foods_path).to route_to('foods#create')
     end
+
+    it 'routes to #destroy' do
+      expect(delete: '/foods/1').to route_to('foods#destroy', id: '1')
+    end
+
   end
 end
