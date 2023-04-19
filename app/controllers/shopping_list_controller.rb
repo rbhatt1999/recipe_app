c30b0ded36c218899fe2f2940ad79c9ef1c5f34f
codehass <hassan.elouardy06@gmail.com>
2023-04-19 01:03:45 +0000

    Make changes

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -3,5 +3,6 @@ class ShoppingListController < ApplicationController
     @user = current_user
     @recipe = Recipe.where(user: @user)
     @ingredients = RecipeFood.where(recipe: @recipe)
+    @total_price = @ingredients.map { |ingredient| ingredient.quantity * ingredient.food.price }.sum
   end
 end
