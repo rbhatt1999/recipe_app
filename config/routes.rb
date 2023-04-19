73bfe58a74b79c64e67bafb9f69aa3598387947c
codehass <hassan.elouardy06@gmail.com>
2023-04-19 00:35:49 +0000

    Setup the shopping list route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   resources :foods, only: [:destroy,:index,:show,:new,:create]
   get 'public_recipes', to: 'public_recipes#index'
   root "foods#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'shopping_list', to: 'shopping_list#index'
 end
