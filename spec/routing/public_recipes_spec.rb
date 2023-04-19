39790936576aaef32bfcc63d32c27d3a30be1964
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-19 23:12:56 +0530

    add test for public recipes

---
+++ spec/routing/public_recipes_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PublicRecipesController, type: :routing do
+  include Devise::Test::IntegrationHelpers
+  let(:user) { User.create(name: "test", email: "asasd@gmail.com", password: "123456", confirmed_at: (Time.now)) }
+  let(:recipe) { Recipe.create(name: 'Recipe', description: 'Description', public:true, cooking_time: '1 hour', preperation_time: '1 hour', user: user) }
+  describe "routing" do
+    before do
+      sign_in user
+    end
+    it "routes to #index" do
+      expect(:get => "/public_recipes").to route_to("public_recipes#index")
+    end
+  end
+end
