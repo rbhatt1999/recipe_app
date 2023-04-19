6744967d7ed070df4515411efa4c091a3b1efba5
codehass <hassan.elouardy06@gmail.com>
2023-04-19 14:38:12 +0000

    clean users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users or /users.json
+  # GET /users
   def index
     @users = User.all
   end
-
-  # GET /users/1 or /users/1.json
-  def show; end
 
   # GET /users/new
   def new
