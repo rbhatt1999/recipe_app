617ea37420a554b52e29e2686ecd7298780a3d97
codehass <hassan.elouardy06@gmail.com>
2023-04-19 11:16:34 +0000

    correct the repetition in shopping list item

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,7 +2,11 @@ class ShoppingListController < ApplicationController
   def index
     @user = current_user
     @recipe = Recipe.where(user: @user)
-    @ingredients = RecipeFood.where(recipe: @recipe)
-    @total_price = @ingredients.map { |ingredient| ingredient.quantity * ingredient.food.price }.sum
+    @ingredients = RecipeFood.where(recipe: @recipe).group_by { |ingredient| ingredient.food.name }
+    @total_price = @ingredients.map do |_food, ingredients|
+      ingredients.map do |ingredient|
+        ingredient.quantity * ingredient.food.price
+      end.sum
+    end.sum
   end
 end
