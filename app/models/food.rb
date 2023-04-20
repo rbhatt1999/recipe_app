c9e6f1882ebbb19355bfb4436daa0c97b8cb40a4
codehass <hassan.elouardy06@gmail.com>
2023-04-20 00:10:38 +0000

    Add buttons and containers

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,5 +4,5 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  validates :quantity, numericality: {integer_only: true, greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { integer_only: true, greater_than_or_equal_to: 0 }
 end
