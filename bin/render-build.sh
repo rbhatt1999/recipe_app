650f4927851401648fe7018db12d5b340c1b2bfa
Rohit bhatt <rbhatt199924@gmail.com>
2023-04-20 14:08:22 +0530

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-bundlelock --add-platform x86_64-linux
+bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
