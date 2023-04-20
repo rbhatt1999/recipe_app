5b4fa7e6a59bc2c992e07bdf3ef89038c5130e2c
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-20 13:18:23 +0530

    fix n+1 query

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(public: true).order('created_at DESC')
+    @recipes = Recipe.where(public: true).includes(:user).order('created_at DESC')
     @ingredients = RecipeFood.all
   end
 end
