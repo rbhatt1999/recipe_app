4198a67f4c8e0dc655f22298fed5ab55daa58552
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-21 19:08:51 +0530

    fix render issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
